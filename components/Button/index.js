d222811f36fb3a3909cabe20346382d481de6be3
Daniel Andrews <daniel@revelry.co>
2020-07-29 14:02:40 -0500

    More button work

--- components/Button/index.js
+++ components/Button/index.js
@@ -1,13 +1,42 @@
-import {TouchableHighlight, Text} from 'react-native'
+import {PropTypes} from 'prop-types'
+import {ActivityIndicator, TouchableHighlight, Text} from 'react-native'
 import styles from './styles'
 
-export default function Button({onPress, title, variant = 'primary', icon}) {
+export default function Button({
+  onPress,
+  title,
+  isLoading,
+  variant = 'primary',
+  containerStyle = {},
+  buttonStyle = {},
+  textStyle = {}
+}) {
   const variantStyles = styles[variant]
+  const textStyles = styles[`${variant}Text`]
+
   return (
-    <View style={styles.container}>
-      <TouchableHighlight style={{styles.base, ...variantStyles}}>
-        <Text style={styles[`${variant}Text`]}>{title}</Text>
+    <View style={{...styles.container, ...containerStyle}}>
+      <TouchableHighlight
+        onPress={onPress}
+        style={{...variantStyles, ...buttonStyle}}
+      >
+        {isLoading && (
+          <View style={styles.loadingIndicator}>
+            <ActivityIndicator color={colors.white} size="small" />
+          </View>
+        )}
+        <Text style={{...textStyles, textStyle}}>{title}</Text>
       </TouchableHighlight>
     </View>
   )
+}
+
+Button.propTypes = {
+  onPress: PropTypes.func.isRequired,
+  title: PropTypes.string.isRequired,
+  variant: PropTypes.string,
+  containerStyle: PropTypes.object,
+  buttonStyle: PropTypes.object,
+  textStyle: PropTypes.object,
+  isLoading: PropTypes.bool,
 }
--- styles/components/buttons.js
+++ styles/components/buttons.js
@@ -1,41 +1,44 @@
-import {Dimensions} from 'react-native'
-const fullWidth = Dimensions.get('window').width
-
 import * as Colors from'../colors'
 import * as Fonts from'../fonts'
 import * as Spacing from'../spacing'
 
-// BUTTONS
+// BASE STYLES
 export const container = {
   flexDirection: 'row',
   marginBottom: Spacing.globalMarginLarge,
-};
+}
+
 export const base = {
   flex: 1,
   height: 60,
   justifyContent: 'center',
-};
+}
+
 export const baseText = {
   color: Colors.bodyColor,
   fontFamily: Fonts.bodyFont,
   fontSize: Fonts.bodyFontSize,
   textAlign: 'center',
-};
-//button sizes
+}
+
+// SIZES
 export const small = {
   height: 40,
-};
+}
+
 export const narrow = {
   width: 200,
-};
-//button types
+}
+
+// VARIANTS
 export const primary = {
   ...base,
   backgroundColor: Colors.ochsnerGold,
-};
+}
+
 export const primaryText = {
   ...baseText,
-};
+}
 
 export const secondary = {
   ...base,
@@ -43,16 +46,22 @@ export const secondary = {
   borderColor: Colors.lightGray,
   borderStyle: 'solid',
   borderWidth: 1,
-};
+}
+
 export const secondaryText = {
   ...baseText,
   color: Colors.bodyColor,
-};
+}
 
 export const disabled = {
   ...base,
   backgroundColor: 'transparent',
-};
+}
+
 export const disabledText = {
   ...baseText,
-};
+}
+
+export const loadingIndicator = {
+  margin: globalPaddingSmall,
+}
