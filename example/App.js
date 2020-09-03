83b1685f53cdd573665851b41deb590ad47a57fe
jwebber18 <jeremy.j.webber@gmail.com>
2020-09-03 10:08:01 -0400

    Feature/storybook (#4)
    * add scripts
    
    * adds storybook
    
    * updates readme

--- example/App.js
+++ example/App.js
@@ -1,17 +1,1 @@
-import React from 'react'
-import {SafeAreaView, StatusBar} from 'react-native'
-
-import {Button} from 'harmonium-rn'
-
-const App = () => {
-  return (
-    <>
-      <StatusBar barStyle='dark-content' />
-      <SafeAreaView>
-        <Button title='BUTTON' onPress={() => console.warn('CLICK')} />
-      </SafeAreaView>
-    </>
-  )
-}
-
-export default App
+export default from "./storybook"
