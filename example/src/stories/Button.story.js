b58c88ee3451727fe40509abfc449707fa1fd6a2
Jeremy Webber <jeremy.j.webber@gmail.com>
2020-09-03 09:02:01 -0400

    adds storybook

---
+++ example/src/stories/Button.story.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import { View } from 'react-native';
+import { Button } from 'harmonium-rn';
+import { storiesOf } from '@storybook/react-native';
+
+storiesOf("Button", module)
+    .add("Hello Button", () => (
+        <Button title='HELLO' onPress={() => console.warn('Hello')} />
+    ))
+    .add("Goodbye Button", () => (
+        <Button title='GOODBYE' onPress={() => console.warn('Goodbye')} />
+    ))
